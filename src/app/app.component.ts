5276a523e7a2ac71972f09279712ec8fba07cb56
Jens Ungerer <github@jens-ungerer.de>
2019-07-15 05:03:35 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mtt-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'mtt-client';
+}
