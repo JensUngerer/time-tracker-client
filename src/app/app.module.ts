1ed38a6673262d44e3d5ae301ba647bbbd4c5620
Jens Ungerer <github@jens-ungerer.de>
2019-07-15 05:16:55 +0200

    several steps in order to setup the angular material + animation + gestures + angular / flex

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+// https://stackoverflow.com/questions/38209713/how-to-make-a-responsive-nav-bar-using-angular-material-2
+// https://material.angular.io/guide/getting-started
+import {FlexLayoutModule} from '@angular/flex-layout';
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import { MatButtonModule } from '@angular/material';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FlexLayoutModule,
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
