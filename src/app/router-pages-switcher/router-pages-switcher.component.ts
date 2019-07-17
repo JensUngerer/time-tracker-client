2b802d7b8ed4b3570702b2ccb40d47599e45f7d0
Jens Ungerer <github@jens-ungerer.de>
2019-07-17 05:40:04 +0200

    adding forward / backward - button logic

--- src/app/router-pages-switcher/router-pages-switcher.component.ts
+++ src/app/router-pages-switcher/router-pages-switcher.component.ts
@@ -1,4 +1,7 @@
+import { RoutingRoutes } from './../routing-routes';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, UrlSegment } from '@angular/router';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Component({
   selector: 'mtt-router-pages-switcher',
@@ -7,9 +10,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RouterPagesSwitcherComponent implements OnInit {
 
-  constructor() { }
+  private currentUrl$: Observable<UrlSegment[]> = null;
+
+  private urlForwardMapping: {[key: string]: string} = {};
+  private urlBackwardMapping: {[key: string]: string} = {};
+  constructor(private activatedRoute: ActivatedRoute,
+              private router: Router) {
+                this.currentUrl$ = this.activatedRoute.url;
+                this.urlBackwardMapping['/' + RoutingRoutes.routes[1]] = null;
+
+                this.urlForwardMapping['/' + RoutingRoutes.routes[1].path] = '/' + RoutingRoutes.routes[2].path;
+                this.urlBackwardMapping['/' + RoutingRoutes.routes[2].path] = '/' + RoutingRoutes.routes[1].path;
+
+
+                this.urlForwardMapping['/' + RoutingRoutes.routes[2].path] = '/' + RoutingRoutes.routes[3].path;
+                this.urlBackwardMapping['/' + RoutingRoutes.routes[3].path] = '/' + RoutingRoutes.routes[2].path;
+
+
+                this.urlForwardMapping['/' + RoutingRoutes.routes[3].path] = '/' + RoutingRoutes.routes[4].path;
+                this.urlBackwardMapping['/' + RoutingRoutes.routes[4].path] = '/' + RoutingRoutes.routes[3].path;
+
+                this.urlForwardMapping['/' + RoutingRoutes.routes[4].path] = null;
+              }
+
+  public isForwardButtonDisabled = false;
+  public isBackwardButtonDisabled = false;
 
   ngOnInit() {
+    this.router.events.subscribe(() => {
+      this.isForwardButtonDisabled = this.checkIsForwardButtonDisabled(this.router.url);
+      this.isBackwardButtonDisabled = this.checkIsBackwardButtonDisabled(this.router.url);
+    });
   }
 
+  private checkIsForwardButtonDisabled(currentUrl: string): boolean {
+    return this.urlForwardMapping[currentUrl] ? false : true;
+  }
+
+  private checkIsBackwardButtonDisabled(currentUrl: string): boolean {
+    return this.urlBackwardMapping[currentUrl] ? false : true;
+  }
+
+  public onForwardButtonClicked() {
+    this.router.navigate([this.urlForwardMapping[this.router.url]]);
+  }
+
+  public onBackwardButtonClicked() {
+    this.router.navigate([this.urlBackwardMapping[this.router.url]]);
+  }
+
+  // private getNextRoute(currentUrl: string): string {
+  //   const nextUrl: string = this.urlForwardMapping[currentUrl];
+  //   return nextUrl;
+  // }
 }
