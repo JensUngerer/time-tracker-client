b00315ca160add3653799c9301ba96f7244d2760
Jens Ungerer <github@jens-ungerer.de>
2019-07-19 11:50:34 +0200

    adding project selection for task in order to have the projecId available

--- src/app/task/task.component.ts
+++ src/app/task/task.component.ts
@@ -1,8 +1,12 @@
+import { IProjectOption, ProjectOption } from './../typescript/projectOption';
+import { ActivatedRoute } from '@angular/router';
 import { CurrentSelectionsPropertiesService } from './../current-selections-properties.service';
 import { TaskService } from './../task.service';
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, AbstractControl, FormControl } from '@angular/forms';
 import { ProjectComponent } from '../project/project.component';
+import { ProjectService } from '../project.service';
+import { IProject } from '../../../../common/typescript/iProject';
 
 @Component({
   selector: 'mtt-task',
@@ -17,10 +21,19 @@ export class TaskComponent implements OnInit {
 
   public isButtonDisabled = false;
 
+  public formControlNameProjectName = 'theProjectName';
+
+  public  projectOptions: IProjectOption[] = [];
+
+
   public onSubmit(values: any) {
     const newNewTaskName = values[this.formControlNameTaskName];
 
-    const projectId = this.currentSelectionsPropertiesService.properties[ProjectComponent.projectIdPropertyName];
+    // const projectId = this.currentSelectionsPropertiesService.properties[ProjectComponent.projectIdPropertyName];
+
+    // const projectId = this.activatedRoute.snapshot.queryParams[ProjectComponent.projectIdPropertyName];
+
+    const projectId = this.taskFormGroup.controls[this.formControlNameProjectName].value.projectId;
 
     this.taskService.addTask(newNewTaskName, projectId);
 
@@ -28,12 +41,24 @@ export class TaskComponent implements OnInit {
   }
 
   constructor(private taskService: TaskService,
-              private currentSelectionsPropertiesService: CurrentSelectionsPropertiesService) {
+              private projectService: ProjectService) {
     const configObj: {[key: string]: AbstractControl} = {};
+
+    configObj[this.formControlNameProjectName] = new FormControl('');
+
 
     configObj[this.formControlNameTaskName] = new FormControl('');
 
     this.taskFormGroup = new FormGroup(configObj);
+
+    const projects = this.projectService.getProjects();
+    if (!projects || projects.length === 0) {
+      console.error('no projects');
+      return;
+    }
+    projects.forEach((element: IProject) => {
+      this.projectOptions.push(new ProjectOption(element));
+    });
   }
 
   ngOnInit() {
