de3a2df0a9e6bbcfd07c0244985cb1594d8ae629
Jens Ungerer <github@jens-ungerer.de>
2019-07-20 13:53:08 +0200

    using routes.js instead of routes.ts

--- src/app/commit.service.ts
+++ src/app/commit.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 // import { IGridCommitLine } from './../../../common/typescript/iGridCommitLine';
-import routesConfig from './../../../common/typescript/routes';
+// import * as routesConfig from './../../../common/typescript/routes.js'; // './../../../common/typescript/routes';
 import { ITimeRecordsDocument } from './../../../common/typescript/mongoDB/iTimeRecordsDocument';
+import * as routes from './../../../common/typescript/routes.js';
 
 @Injectable({
   providedIn: 'root'
@@ -13,9 +14,9 @@ export class CommitService {
 
   public postCommit(line: ITimeRecordsDocument): Promise<any> {
     return new Promise<any>((resolve: (value: any) => void) => {
-      const url = 'http://localhost:' + routesConfig.port + routesConfig.timeRecord;
+      const url = 'http://localhost:' + routes.port + routes.timeRecord;
       const body: any = {};
-      body[routesConfig.timeRecordBodyProperty] = line;
+      body[routes.timeRecordBodyProperty] = line;
       const options: any = {
         'Content-Type': 'application/json'
       };
